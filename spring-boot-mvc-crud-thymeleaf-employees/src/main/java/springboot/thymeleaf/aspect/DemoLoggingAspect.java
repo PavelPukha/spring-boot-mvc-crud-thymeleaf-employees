0554f3c11ef6deb68d0d8a1f2827a47b0cd8b1c6
PavelPukha <139925818+PavelPukha@users.noreply.github.com>
2024-06-17 00:20:58 +0200

    Clearing unnecessary comments

--- spring-boot-mvc-crud-thymeleaf-employees/src/main/java/springboot/thymeleaf/aspect/DemoLoggingAspect.java
+++ spring-boot-mvc-crud-thymeleaf-employees/src/main/java/springboot/thymeleaf/aspect/DemoLoggingAspect.java
@@ -13,14 +13,11 @@ import java.util.logging.Logger;
 @Component
 public class DemoLoggingAspect {
 
-    //set logger
     private Logger myLogger = Logger.getLogger(getClass().getName());
 
-    //setup pointcut declarations
     @Pointcut("execution(* springboot.thymeleaf.controller.*.*(..))")
     private void forControllerPackage() {}
 
-    //do the same for service and dao
     @Pointcut("execution(* springboot.thymeleaf.service.*.*(..))")
     private void forServicePackage() {}
 
@@ -30,38 +27,30 @@ public class DemoLoggingAspect {
     @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
     private void forAppFlow() {}
 
-    // add @Before advice
 
     @Before("forAppFlow()")
     public void before(JoinPoint theJoinPoint) {
 
-        // display method we are calling
         String theMethod = theJoinPoint.getSignature().toShortString();
         myLogger.info("=====>> in @Before: calling method: " + theMethod);
 
-        // display the arguments to the method
-
-        //get the arguments
         Object[] args = theJoinPoint.getArgs();
 
 
-        //loop thru and display args
         for (Object tempArg : args) {
             myLogger.info("====>> argument: " + tempArg);
         }
     }
-        // add @AfterReturning advice
+    
     @AfterReturning(
             pointcut = "forAppFlow()",
             returning = "theResult")
     public void afterReturning(JoinPoint theJoinPoint,Object theResult) {
 
-        // display method we are returning from
         String theMethod = theJoinPoint.getSignature().toShortString();
         myLogger.info("=====>> in @AfterReturning: from method: " + theMethod);
 
 
-        // display data returned
         myLogger.info("======>> result: " + theResult);
 
     }
